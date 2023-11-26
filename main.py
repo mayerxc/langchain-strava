39a3ccaf96c12faf0808d13a9ee3d95379da7543
Chris Mayer <mayeerxc@gmail.com>
2023-11-26 18:15:55 -0500

    WIP

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import os
-import sys
-
-# from langchain.llms import OpenAI
-# from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, JSONLoader, TextLoader
+from langchain.document_loaders import DirectoryLoader, JSONLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.vectorstores import utils as chroma_utils
 
+# from langchain.retrievers.self_query.base import SelfQueryRetriever
+# from langchain.llms import OpenAI
 # from langchain.chains import ConversationalRetrievalChain
 
 
@@ -20,22 +19,44 @@ load_dotenv()
 os.getenv("OPENAI_API_KEY")
 
 # llm = OpenAI()
-chat_model = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+# temperature=0.5
+chat_model = ChatOpenAI(model_name="gpt-3.5-turbo")
 
 
+# Define the metadata extraction function.
+def metadata_func(record: dict, metadata: dict) -> dict:
+    metadata["distance_in_meters"] = record.get("distance")
+    metadata["start_date_local"] = record.get("start_date_local")
+    metadata["moving_time_in_seconds"] = record.get("moving_time")
+    metadata["location_city"] = record.get("location_city")
+    metadata["location_state"] = record.get("location_state")
+    metadata["location_country"] = record.get("location_country")
+    metadata["sport_type"] = record.get("sport_type")
+    metadata["average_heartrate_in_beats_per_minute"] = record.get("average_heartrate")
+    metadata["max_heartrate"] = record.get("max_heartrate")
+    metadata["total_elevation_gain_in_meters"] = record.get("total_elevation_gain")
+    return metadata
+
+
+json_loader_kwargs = {
+    "jq_schema": ".[]",
+    "content_key": "name",
+    "metadata_func": metadata_func,
+}
 loader = DirectoryLoader(
     "data/",
     loader_cls=JSONLoader,
     glob="**/*.json",
-    loader_kwargs={
-        "jq_schema": ".[].name",
-    },
+    loader_kwargs=json_loader_kwargs,
 )
 # loader2 = DirectoryLoader("data/", loader_cls=TextLoader)
 
 docs = loader.load()
+# filter docs because of unexpected metadata error for ChromaDB
+filtered_metadata_docs = chroma_utils.filter_complex_metadata(docs)
 # docs = loader2.load()
 # print(f"docs count {len(docs)}")
+
 print(f"docs: {docs}")
 
 
@@ -45,15 +66,16 @@ if os.path.exists("persist"):
         persist_directory="persist", embedding_function=OpenAIEmbeddings()
     )
     index = VectorStoreIndexWrapper(vectorstore=vectorstore)
+    # retriever = vectorstore.as_retriever()
 else:
     print("else")
     index = VectorstoreIndexCreator(
-        # vectorstore_cls=Chroma,
-        # embedding=OpenAIEmbeddings(),
+        vectorstore_cls=Chroma,
+        embedding=OpenAIEmbeddings(),
         vectorstore_kwargs={"persist_directory": "persist"},
-    ).from_loaders([loader])
+    ).from_documents(filtered_metadata_docs)
 query = input("Prompt: ")
-result = index.query(query)
+result = index.query_with_sources(query)
 print(result)
 
 # result = chat_model.predict(query)
