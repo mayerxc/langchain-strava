3b38fce99177ce6c88de7df593fb81ca4b83f5db
Chris Mayer <mayeerxc@gmail.com>
2023-11-26 20:40:03 -0500

    Trying Self Query Retriever

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import DirectoryLoader, JSONLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.vectorstores import utils as chroma_utils
-
-# from langchain.retrievers.self_query.base import SelfQueryRetriever
-# from langchain.llms import OpenAI
-# from langchain.chains import ConversationalRetrievalChain
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
 
 
 # load all environmental variables
@@ -18,25 +14,84 @@ load_dotenv()
 # load api key into app
 os.getenv("OPENAI_API_KEY")
 
-# llm = OpenAI()
-# temperature=0.5
+# gpt-4
+# gpt-3.5-turbo
 chat_model = ChatOpenAI(model_name="gpt-3.5-turbo")
 
 
 # Define the metadata extraction function.
 def metadata_func(record: dict, metadata: dict) -> dict:
     metadata["distance_in_meters"] = record.get("distance")
+    metadata["average_speed"] = record.get("average_speed")
     metadata["start_date_local"] = record.get("start_date_local")
     metadata["moving_time_in_seconds"] = record.get("moving_time")
     metadata["location_city"] = record.get("location_city")
     metadata["location_state"] = record.get("location_state")
     metadata["location_country"] = record.get("location_country")
     metadata["sport_type"] = record.get("sport_type")
-    metadata["average_heartrate_in_beats_per_minute"] = record.get("average_heartrate")
+    metadata["average_heartrate"] = record.get("average_heartrate")
     metadata["max_heartrate"] = record.get("max_heartrate")
     metadata["total_elevation_gain_in_meters"] = record.get("total_elevation_gain")
     return metadata
 
+
+metadata_field_info = [
+    AttributeInfo(
+        name="distance_in_meters",
+        description="The distance of the run or workout in meters",
+        type="float",
+    ),
+    AttributeInfo(
+        name="average_speed",
+        description="The average speed of the run or workout in meters per second",
+        type="float",
+    ),
+    AttributeInfo(
+        name="start_date_local",
+        description="Time of day that the workout started",
+        type="date",
+    ),
+    AttributeInfo(
+        name="moving_time_in_seconds",
+        description="The duration of the workout",
+        type="float",
+    ),
+    AttributeInfo(
+        name="location_city",
+        description="The city where the workout occurred",
+        type="string",
+    ),
+    AttributeInfo(
+        name="location_state",
+        description="The state inside a country where the workout occurred",
+        type="string",
+    ),
+    AttributeInfo(
+        name="location_country",
+        description="The country where the workout occurred",
+        type="string",
+    ),
+    AttributeInfo(
+        name="sport_type",
+        description="The type of sport that the GPS watch recorded",
+        type="string",
+    ),
+    AttributeInfo(
+        name="average_heartrate",
+        description="Average heart rate in beats per minute",
+        type="float",
+    ),
+    AttributeInfo(
+        name="max_heartrate",
+        description="Maximum heart rate in beats per minute that occurred during the workout",
+        type="float",
+    ),
+    AttributeInfo(
+        name="total_elevation_gain_in_meters",
+        description="The elevation gain of the run or workout",
+        type="float",
+    ),
+]
 
 json_loader_kwargs = {
     "jq_schema": ".[]",
@@ -44,55 +99,32 @@ json_loader_kwargs = {
     "metadata_func": metadata_func,
 }
 loader = DirectoryLoader(
-    "data/",
+    "data2/",
     loader_cls=JSONLoader,
     glob="**/*.json",
     loader_kwargs=json_loader_kwargs,
 )
-# loader2 = DirectoryLoader("data/", loader_cls=TextLoader)
 
 docs = loader.load()
 # filter docs because of unexpected metadata error for ChromaDB
 filtered_metadata_docs = chroma_utils.filter_complex_metadata(docs)
-# docs = loader2.load()
-# print(f"docs count {len(docs)}")
 
-print(f"docs: {docs}")
+print(f"docs: {docs[0]}")
 
 
-if os.path.exists("persist"):
-    print("os path")
-    vectorstore = Chroma(
-        persist_directory="persist", embedding_function=OpenAIEmbeddings()
-    )
-    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-    # retriever = vectorstore.as_retriever()
-else:
-    print("else")
-    index = VectorstoreIndexCreator(
-        vectorstore_cls=Chroma,
-        embedding=OpenAIEmbeddings(),
-        vectorstore_kwargs={"persist_directory": "persist"},
-    ).from_documents(filtered_metadata_docs)
-query = input("Prompt: ")
-result = index.query_with_sources(query)
-print(result)
+vectorstore = Chroma(
+    persist_directory="persist", embedding_function=OpenAIEmbeddings()
+).from_documents(filtered_metadata_docs, OpenAIEmbeddings())
 
-# result = chat_model.predict(query)
-# print(result)
 
-# chain = ConversationalRetrievalChain.from_llm(
-#     llm=ChatOpenAI(model="gpt-3.5-turbo"),
-#     retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-# )
-# chat_history = []
-# while True:
-#     if not query:
-#         query = input("Prompt: ")
-#     if query in ["quit", "q", "exit"]:
-#         sys.exit()
-#     result = chain({"question": query, "chat_history": chat_history})
-#     print(result["answer"])
+retriever = SelfQueryRetriever.from_llm(
+    llm=chat_model,
+    vectorstore=vectorstore,
+    document_contents="Workout data from a GPS watch",
+    metadata_field_info=metadata_field_info,
+)
+prompt = input("Prompt: ")
 
-#     chat_history.append((query, result["answer"]))
-#     query = None
+found_docs = retriever.invoke(prompt)
+
+print(found_docs)
